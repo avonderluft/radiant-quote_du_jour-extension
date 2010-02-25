2f46b24b16927254528a41a6408eb4ef6be92211
Andrew vonderLuft <avonderluft@avlux.net>
2010-02-25 09:03:34 -0800

    enable specific quote to be rendered by id

--- lib/quote_du_jour/tags.rb
+++ lib/quote_du_jour/tags.rb
@@ -2,20 +2,25 @@ module QuoteDuJour::Tags
   include Radiant::Taggable
 
   desc %{
-    Renders a random quote from the quotes table.  If the author attribute is included,
+    Renders a random quote from the quotes table (or if the id attribute is included,
+    that specific quote).  If the id attribute is not present, and the author attribute is included,
     then the random quote will be selected from those available by that author.
     
     *Usage*:
 
-    <pre><code><r:quote [author="author_name" all="true"] /></code></pre>
+    <pre><code><r:quote [id="quote_id" || author="author_name" all="true"] /></code></pre>
   }
   tag "quote" do |tag|
-    if tag.attr['author']
-      quotes = Quote.find_all_by_author(tag.attr['author'])
+    if tag.attr['id']
+      quote = Quote.find(tag.attr['id'])
     else
-      quotes = Quote.find(:all, :order => "author asc")
+      if tag.attr['author']
+        quotes = Quote.find_all_by_author(tag.attr['author'])
+      else
+        quotes = Quote.find(:all, :order => "author asc")
+      end
+      quote = quotes[rand(quotes.length)]
     end
-    quote = quotes[rand(quotes.length)]
     result = '<blockquote>' + quote.quote
     result << "<br /><br /><cite>&nbsp;&mdash;&nbsp;" + quote.author
     result << " in " + quote.work unless quote.work == ""
